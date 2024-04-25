503c38f71ec2a9c63fd4afb2643da68e50bf9c99
Akash <akashpalvibrant@gmail.com>
2024-04-25 17:52:25 +0530

    Kt

---
+++ app/src/main/java/com/school/apicallskt/retrofit_kotlin/activity/SinglePostKtActivity.kt
@@ -0,0 +1,24 @@
+package com.school.apicallskt.retrofit_kotlin.activity
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.school.apicallskt.R
+
+class SinglePostKtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_single_post_kt)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val id: String? =intent.getStringExtra("id")
+        Toast.makeText(this,id,Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/school/apicallskt/retrofit_kotlin/adapter/PostKtAdapter.kt
+++ app/src/main/java/com/school/apicallskt/retrofit_kotlin/adapter/PostKtAdapter.kt
@@ -1,25 +1,23 @@
 package com.school.apicallskt.retrofit_kotlin.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.school.apicallskt.R
+import com.school.apicallskt.retrofit_kotlin.activity.SinglePostKtActivity
 import com.school.apicallskt.retrofit_kotlin.model.PostKtModel
 
-class PostKtAdapter( val context: Context,val postList: List<PostKtModel>) : RecyclerView.Adapter<PostKtAdapter.MyViewHolder>() {
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-       var userId:TextView=itemView.findViewById(R.id.userId)
-       var id:TextView=itemView.findViewById(R.id.Id)
-       var body:TextView=itemView.findViewById(R.id.body)
-       var title:TextView=itemView.findViewById(R.id.tittle)
-
-    }
-
+class PostKtAdapter(val context: Context, val postList: List<PostKtModel>) :
+    RecyclerView.Adapter<PostKtAdapter.MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.post_list_items,parent,false))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.post_list_items, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
@@ -27,10 +25,25 @@ class PostKtAdapter( val context: Context,val postList: List<PostKtModel>) : Rec
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val model=postList[position]
-        holder.userId.text=model.userId.toString()
-        holder.id.text=model.id.toString()
-        holder.body.text=model.body
-        holder.title.text=model.title
+        val model = postList[position]
+        holder.userId.text = model.userId.toString()
+        holder.id.text = model.id.toString()
+        holder.body.text = model.body
+        holder.title.text = model.title
+        holder.card_item.setOnClickListener({
+            val intent=Intent(context,SinglePostKtActivity::class.java)
+            intent.putExtra("id",model.id.toString())
+            context.startActivity(intent)
+        })
+
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var userId: TextView = itemView.findViewById(R.id.userId)
+        var id: TextView = itemView.findViewById(R.id.Id)
+        var body: TextView = itemView.findViewById(R.id.body)
+        var title: TextView = itemView.findViewById(R.id.tittle)
+        var card_item: MaterialCardView = itemView.findViewById(R.id.card_item)
+
     }
 }
